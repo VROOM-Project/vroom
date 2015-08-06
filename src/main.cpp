c944762c4b623c8ba8044f6c9e228a9db862825c
jcoupey <git@coupey.fr>
2015-08-06 16:04:35 +0200

    Adding main and makefile.

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+/*
+VROOM (Vehicle Routing Open-source Optimization Machine)
+Copyright (C) 2015, Julien Coupey
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <string>
+#include "./heuristics/tsp_strategy.h"
+
+int main(int argc, char **argv){
+
+  std::string request = argv[1];
+  std::string out_file = "solution.json";
+  
+  solve_atsp(request, out_file);
+
+  return 0;
+}
