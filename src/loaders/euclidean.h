30dcd38b986727081072f62193d2758b3df93175
jcoupey <git@coupey.fr>
2016-01-23 22:13:35 +0100

    Cleanup euclidean loader. #23

--- src/loaders/euclidean.h
+++ src/loaders/euclidean.h
@@ -19,6 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef EUCLIDEAN_H
 #define EUCLIDEAN_H
 #include <vector>
+#include <regex>
 #include "./problem_io.h"
 #include "../structures/matrix.h"
 #include "../utils/exceptions.h"
@@ -28,6 +29,24 @@ class euclidean : public problem_io<distance_t>{
 private:
   std::vector<std::pair<double, double>> _locations;
 
+  void add_location(const std::string location){
+    // Regex check for valid location.
+    std::regex valid_loc ("loc=-?[0-9]+\\.?[0-9]*,-?[0-9]+\\.?[0-9]*[[:space:]]*");
+    if(!std::regex_match(location, valid_loc)){
+      throw custom_exception("invalid syntax for location "
+                             + std::to_string(_locations.size() + 1)
+                             + ", see vroom -h for usage display."
+                             );
+    }
+
+    // Parsing the location is now safe.
+    std::size_t separator_rank = location.find(",");
+    std::string lat = location.substr(4, separator_rank);
+    std::string lon = location.substr(separator_rank + 1, location.length() -1);
+    _locations.emplace_back(std::stod(lat, nullptr),
+                            std::stod(lon, nullptr));
+  }
+  
 public:
   euclidean(std::string loc_input){
     // Parsing input in locations.
@@ -48,18 +67,32 @@ public:
   }
 
   virtual matrix<distance_t> get_matrix() const override{
-    return virtual_euclidian_matrix<distance_t>(_locations);
+    return matrix<distance_t>(_locations);
   }
 
   virtual void get_route(const std::list<index_t>& tour,
                          rapidjson::Value& value,
                          rapidjson::Document::AllocatorType& allocator) const override{
     rapidjson::Value route_array(rapidjson::kArrayType);
+    for(auto const& step: tour){
+      route_array
+        .PushBack(rapidjson::Value(rapidjson::kArrayType)
+                  .PushBack(_locations[step].first, allocator)
+                  .PushBack(_locations[step].second, allocator),
+                  allocator);
+    }
+    value.Swap(route_array);
   }
 
   virtual void get_tour(const std::list<index_t>& tour,
                         rapidjson::Value& value,
                         rapidjson::Document::AllocatorType& allocator) const override{
+    rapidjson::Value tour_array(rapidjson::kArrayType);
+    for(auto const& step: tour){
+      // Using input index to describe locations.
+      tour_array.PushBack(step, allocator);
+    }
+    value.Swap(tour_array);
   }
 
   virtual void get_route_infos(const std::list<index_t>& tour,
