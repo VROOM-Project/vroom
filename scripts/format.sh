8cf07f50d882f30b2f033cf5f825d5ce1226e56d
jcoupey <git@coupey.fr>
2017-10-23 21:36:16 +0200

    Add clang-format wrapper script.

---
+++ scripts/format.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Borrowed and slightly adapted from
+# https://github.com/Project-OSRM/osrm-backend/blob/master/scripts/format.sh
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# Runs the Clang Formatter in parallel on the code base.
+# Return codes:
+#  - 1 there are files to be formatted or clang-format 3.8 is missing
+#  - 0 everything looks fine
+
+# Get CPU count
+OS=$(uname)
+NPROC=1
+if [[ $OS = "Linux" ]] ; then
+    NPROC=$(nproc)
+elif [[ ${OS} = "Darwin" ]] ; then
+    NPROC=$(sysctl -n hw.physicalcpu)
+fi
+
+# Discover clang-format
+if type clang-format-3.8 2> /dev/null ; then
+    CLANG_FORMAT=clang-format-3.8
+elif type clang-format 2> /dev/null ; then
+    # Clang format found, but need to check version
+    CLANG_FORMAT=clang-format
+    V=$(clang-format --version)
+    if [[ $V != *3.8* ]] ; then
+        echo "clang-format is not 3.8 (returned ${V})"
+        exit 1
+    fi
+else
+    echo "No appropriate clang-format found (expected clang-format-3.8, or clang-format)"
+    exit 1
+fi
+
+find src -type f -name '*.h' -o -name '*.cpp' \
+  | xargs -I{} -P ${NPROC} ${CLANG_FORMAT} -i -style=file {}
