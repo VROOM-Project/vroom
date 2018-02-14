963e282ec732f23d4d3a5dbf276a7e1c7bf6920a
jcoupey <git@coupey.fr>
2018-02-14 12:38:35 +0100

    Minimal failing example.

--- examples/libvroom.cpp
+++ examples/libvroom.cpp
@@ -23,6 +23,29 @@ int main() {
   input problem_instance(std::move(routing_wrapper),
                          true); // Query for route geometry after solving.
 
+  matrix<cost_t> matrix_input(4);
+  matrix_input[0][0] = 0;
+  matrix_input[0][1] = 0;
+  matrix_input[0][2] = 775;
+  matrix_input[0][3] = 1340;
+
+  matrix_input[1][0] = 0;
+  matrix_input[1][1] = 0;
+  matrix_input[1][2] = 775;
+  matrix_input[1][3] = 1340;
+
+  matrix_input[2][0] = 746;
+  matrix_input[2][1] = 746;
+  matrix_input[2][2] = 0;
+  matrix_input[2][3] = 1635;
+
+  matrix_input[3][0] = 1507;
+  matrix_input[3][1] = 1507;
+  matrix_input[3][2] = 1593;
+  matrix_input[3][3] = 0;
+
+  problem_instance._matrix = matrix_input;
+
   // Define vehicle with id, start and end (use boost::none for no
   // start or no end).
   vehicle_t v(0, location_t({2.3526, 48.8604}), location_t({2.3526, 48.8604}));
