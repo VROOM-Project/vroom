dd8c805ff137ab51624a2d3e950c8cb23e5e51b4
jcoupey <git@coupey.fr>
2018-03-13 10:28:38 +0100

    Remove checks against boost::none.

--- src/structures/vroom/vehicle.cpp
+++ src/structures/vroom/vehicle.cpp
@@ -20,20 +20,20 @@ vehicle_t::vehicle_t(ID_t id,
                      const boost::optional<location_t>& end,
                      const boost::optional<amount_t>& capacity)
   : id(id), start(start), end(end), capacity(capacity) {
-  if ((start == boost::none) and (end == boost::none)) {
+  if (!static_cast<bool>(start) and !static_cast<bool>(end)) {
     throw custom_exception("No start or end specified for vehicle " +
                            std::to_string(id) + '.');
   }
 }
 
 bool vehicle_t::has_start() const {
-  return start != boost::none;
+  return static_cast<bool>(start);
 }
 
 bool vehicle_t::has_end() const {
-  return end != boost::none;
+  return static_cast<bool>(end);
 }
 
 bool vehicle_t::has_capacity() const {
-  return capacity != boost::none;
+  return static_cast<bool>(capacity);
 }
