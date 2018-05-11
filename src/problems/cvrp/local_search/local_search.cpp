45307d17c8e0a436fb0adbc862f47ea250eb4819
jcoupey <git@coupey.fr>
2018-05-11 08:16:26 +0200

    Narrow down source choices in local search operators.

--- src/problems/cvrp/local_search/local_search.cpp
+++ src/problems/cvrp/local_search/local_search.cpp
@@ -15,10 +15,88 @@ All rights reserved (see LICENSE).
 #include "or_opt.h"
 #include "relocate.h"
 
+index_t cvrp_local_search::previous_index(index_t route_rank,
+                                          index_t job_rank) const {
+  if (job_rank == 0) {
+    // Only deal with those cases for now.
+    assert(_input._vehicles[route_rank].has_start());
+    return _input._vehicles[route_rank].start.get().index();
+  } else {
+    return _input._jobs[_sol[route_rank][job_rank - 1]].index();
+  }
+}
+
+index_t cvrp_local_search::next_index(index_t route_rank,
+                                      index_t job_rank) const {
+  if (job_rank == _sol[route_rank].size() - 1) {
+    assert(_input._vehicles[route_rank].has_end());
+    return _input._vehicles[route_rank].end.get().index();
+  } else {
+    return _input._jobs[_sol[route_rank][job_rank + 1]].index();
+  }
+}
+
+void cvrp_local_search::set_node_candidate(index_t v) {
+  auto m = _input.get_matrix();
+  gain_t best_gain = std::numeric_limits<gain_t>::min();
+  for (std::size_t i = 0; i < _sol[v].size(); ++i) {
+    // Compute potential gain to relocate current job.
+    index_t previous = previous_index(v, i);
+    index_t current = _input._jobs[_sol[v][i]].index();
+    index_t next = next_index(v, i);
+
+    gain_t new_edge_cost = m[previous][next];
+    if (_sol[v].size() == 1) {
+      // Trying to empty a route, so cost of start --> end without
+      // job is not taken into account.
+      new_edge_cost = 0;
+    }
+    gain_t current_gain =
+      m[previous][current] + m[current][next] - new_edge_cost;
+
+    if (current_gain > best_gain) {
+      best_gain = current_gain;
+      _node_candidates[v] = i;
+    }
+  }
+}
+
+void cvrp_local_search::set_edge_candidate(index_t v) {
+  auto m = _input.get_matrix();
+  gain_t best_gain = std::numeric_limits<gain_t>::min();
+
+  std::size_t nb_edges = (_sol[v].size() < 2) ? 0 : _sol[v].size() - 1;
+
+  for (std::size_t i = 0; i < nb_edges; ++i) {
+    // Compute potential gain to relocate edge from current to next
+    // job.
+    index_t previous = previous_index(v, i);
+    index_t current = _input._jobs[_sol[v][i]].index();
+    index_t after_current = _input._jobs[_sol[v][i + 1]].index();
+    index_t next = next_index(v, i + 1);
+
+    gain_t new_edge_cost = m[previous][next];
+    if (nb_edges == 2) {
+      // Trying to empty a route, so cost of start --> end without
+      // job is not taken into account.
+      new_edge_cost = 0;
+    }
+    gain_t current_gain =
+      m[previous][current] + m[after_current][next] - new_edge_cost;
+
+    if (current_gain > best_gain) {
+      best_gain = current_gain;
+      _edge_candidates[v] = i;
+    }
+  }
+}
+
 cvrp_local_search::cvrp_local_search(const input& input, raw_solution& sol)
   : _input(input),
     _sol(sol),
-    _amounts(sol.size(), amount_t(input.amount_size())) {
+    _amounts(sol.size(), amount_t(input.amount_size())),
+    _node_candidates(_input._vehicles.size()),
+    _edge_candidates(_input._vehicles.size()) {
 
   std::cout << "Amount lower bound: ";
   auto amount_lower_bound = _input.get_amount_lower_bound();
@@ -33,11 +111,19 @@ cvrp_local_search::cvrp_local_search(const input& input, raw_solution& sol)
     }
     auto& capacity = _input._vehicles[i].capacity;
 
-    std::cout << "Amount for vehicle at rank " << i << ": ";
+    std::cout << "Amount for vehicle " << _input._vehicles[i].id << " (at rank "
+              << i << "): ";
     for (std::size_t r = 0; r < _amounts[i].size(); ++r) {
       std::cout << _amounts[i][r] << " / " << capacity[r] << " ; ";
     }
     std::cout << std::endl;
+  }
+
+  // Find best candidate for job pop in each route.
+  auto V = _input._vehicles.size();
+  for (std::size_t v = 0; v < V; ++v) {
+    set_node_candidate(v);
+    set_edge_candidate(v);
   }
 }
 
@@ -55,8 +141,8 @@ void cvrp_local_search::run() {
 
   // List of source/target pairs we need to test (all at first).
   std::vector<std::pair<index_t, index_t>> s_t_pairs;
-  for (unsigned s_v = 0; s_v < _sol.size(); ++s_v) {
-    for (unsigned t_v = 0; t_v < _sol.size(); ++t_v) {
+  for (unsigned s_v = 0; s_v < V; ++s_v) {
+    for (unsigned t_v = 0; t_v < V; ++t_v) {
       if (s_v == t_v) {
         continue;
       }
@@ -86,35 +172,29 @@ void cvrp_local_search::run() {
       if (_sol[s_t.first].size() == 0) {
         continue;
       }
-      for (unsigned s_rank = 0; s_rank < _sol[s_t.first].size(); ++s_rank) {
-        for (unsigned t_rank = 0; t_rank <= _sol[s_t.second].size(); ++t_rank) {
-          relocate
-            r(_input, _sol, _amounts, s_t.first, s_rank, s_t.second, t_rank);
-          if (r.gain() > best_gains[s_t.first][s_t.second] and r.is_valid()) {
-            best_gains[s_t.first][s_t.second] = r.gain();
-            best_ops[s_t.first][s_t.second] = std::make_unique<relocate>(r);
-          }
+      auto s_rank = _node_candidates[s_t.first];
+      for (unsigned t_rank = 0; t_rank <= _sol[s_t.second].size(); ++t_rank) {
+        relocate
+          r(_input, _sol, _amounts, s_t.first, s_rank, s_t.second, t_rank);
+        if (r.gain() > best_gains[s_t.first][s_t.second] and r.is_valid()) {
+          best_gains[s_t.first][s_t.second] = r.gain();
+          best_ops[s_t.first][s_t.second] = std::make_unique<relocate>(r);
         }
       }
     }
 
     // Exchange stuff
     for (const auto& s_t : s_t_pairs) {
-      if (s_t.second <= s_t.first) {
-        // This operator is symmetric.
-        continue;
-      }
       if ((_sol[s_t.first].size() == 0) or (_sol[s_t.second].size() == 0)) {
         continue;
       }
-      for (unsigned s_rank = 0; s_rank < _sol[s_t.first].size(); ++s_rank) {
-        for (unsigned t_rank = 0; t_rank < _sol[s_t.second].size(); ++t_rank) {
-          exchange
-            r(_input, _sol, _amounts, s_t.first, s_rank, s_t.second, t_rank);
-          if (r.gain() > best_gains[s_t.first][s_t.second] and r.is_valid()) {
-            best_gains[s_t.first][s_t.second] = r.gain();
-            best_ops[s_t.first][s_t.second] = std::make_unique<exchange>(r);
-          }
+      auto s_rank = _node_candidates[s_t.first];
+      for (unsigned t_rank = 0; t_rank < _sol[s_t.second].size(); ++t_rank) {
+        exchange
+          r(_input, _sol, _amounts, s_t.first, s_rank, s_t.second, t_rank);
+        if (r.gain() > best_gains[s_t.first][s_t.second] and r.is_valid()) {
+          best_gains[s_t.first][s_t.second] = r.gain();
+          best_ops[s_t.first][s_t.second] = std::make_unique<exchange>(r);
         }
       }
     }
@@ -129,37 +209,29 @@ void cvrp_local_search::run() {
       if (_sol[s_t.first].size() < 2) {
         continue;
       }
-      for (unsigned s_rank = 0; s_rank < _sol[s_t.first].size() - 1; ++s_rank) {
-        for (unsigned t_rank = 0; t_rank <= _sol[s_t.second].size(); ++t_rank) {
-          or_opt
-            r(_input, _sol, _amounts, s_t.first, s_rank, s_t.second, t_rank);
-          if (r.gain() > best_gains[s_t.first][s_t.second] and r.is_valid()) {
-            best_gains[s_t.first][s_t.second] = r.gain();
-            best_ops[s_t.first][s_t.second] = std::make_unique<or_opt>(r);
-          }
+      auto s_rank = _edge_candidates[s_t.first];
+      for (unsigned t_rank = 0; t_rank <= _sol[s_t.second].size(); ++t_rank) {
+        or_opt r(_input, _sol, _amounts, s_t.first, s_rank, s_t.second, t_rank);
+        if (r.gain() > best_gains[s_t.first][s_t.second] and r.is_valid()) {
+          best_gains[s_t.first][s_t.second] = r.gain();
+          best_ops[s_t.first][s_t.second] = std::make_unique<or_opt>(r);
         }
       }
     }
 
     // CROSS-exchange stuff
     for (const auto& s_t : s_t_pairs) {
-      if (s_t.second <= s_t.first) {
-        // This operator is symmetric.
-        continue;
-      }
       if ((_sol[s_t.first].size() < 2) or (_sol[s_t.second].size() < 2)) {
         continue;
       }
-      for (unsigned s_rank = 0; s_rank < _sol[s_t.first].size() - 1; ++s_rank) {
-        for (unsigned t_rank = 0; t_rank < _sol[s_t.second].size() - 1;
-             ++t_rank) {
-          cross_exchange
-            r(_input, _sol, _amounts, s_t.first, s_rank, s_t.second, t_rank);
-          if (r.gain() > best_gains[s_t.first][s_t.second] and r.is_valid()) {
-            best_gains[s_t.first][s_t.second] = r.gain();
-            best_ops[s_t.first][s_t.second] =
-              std::make_unique<cross_exchange>(r);
-          }
+      auto s_rank = _edge_candidates[s_t.first];
+      for (unsigned t_rank = 0; t_rank < _sol[s_t.second].size() - 1;
+           ++t_rank) {
+        cross_exchange
+          r(_input, _sol, _amounts, s_t.first, s_rank, s_t.second, t_rank);
+        if (r.gain() > best_gains[s_t.first][s_t.second] and r.is_valid()) {
+          best_gains[s_t.first][s_t.second] = r.gain();
+          best_ops[s_t.first][s_t.second] = std::make_unique<cross_exchange>(r);
         }
       }
     }
@@ -169,8 +241,8 @@ void cvrp_local_search::run() {
     index_t best_source = 0;
     index_t best_target = 0;
 
-    for (unsigned s_v = 0; s_v < _sol.size(); ++s_v) {
-      for (unsigned t_v = 0; t_v < _sol.size(); ++t_v) {
+    for (unsigned s_v = 0; s_v < V; ++s_v) {
+      for (unsigned t_v = 0; t_v < V; ++t_v) {
         if (s_v == t_v) {
           continue;
         }
@@ -196,12 +268,18 @@ void cvrp_local_search::run() {
       //               false,
       //               "ls_log_" + std::to_string(ls_step) + "_sol.json");
 
+      // Update candidates.
+      set_node_candidate(best_source);
+      set_node_candidate(best_target);
+      set_edge_candidate(best_source);
+      set_edge_candidate(best_target);
+
       // Set gains to zero for what needs to be recomputed in the next round.
       s_t_pairs.clear();
       best_gains[best_source] = std::vector<gain_t>(V, 0);
       best_gains[best_target] = std::vector<gain_t>(V, 0);
 
-      for (unsigned v = 0; v < _sol.size(); ++v) {
+      for (unsigned v = 0; v < V; ++v) {
         if (v != best_source) {
           s_t_pairs.emplace_back(best_source, v);
           s_t_pairs.emplace_back(v, best_source);
