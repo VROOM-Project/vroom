f312f32a9841c9ab85c4a74531fd4ed4d0d8298e
jcoupey <git@coupey.fr>
2018-08-27 19:48:00 +0200

    Lots of (multi-threaded) heuristic parameters.

--- src/problems/vrptw/vrptw.cpp
+++ src/problems/vrptw/vrptw.cpp
@@ -7,9 +7,11 @@ All rights reserved (see LICENSE).
 
 */
 
-#include "problems/vrptw/vrptw.h"
+#include <thread>
+
 #include "problems/vrptw/heuristics/best_insertion.h"
 #include "problems/vrptw/heuristics/solomon.h"
+#include "problems/vrptw/vrptw.h"
 #include "structures/vroom/input/input.h"
 #include "structures/vroom/tw_route.h"
 #include "utils/helpers.h"
@@ -20,8 +22,59 @@ vrptw::vrptw(const input& input) : vrp(input) {
 }
 
 solution vrptw::solve(unsigned exploration_level, unsigned nb_threads) const {
-  // tw_solution sol = best_insertion(_input);
-  tw_solution sol = solomon(_input, 1);
+  struct param {
+    INIT_T init;
+    float regret_coeff;
+  };
+  std::vector<param> parameters;
 
-  return format_solution(_input, sol);
+  std::vector<INIT_T> inits({INIT_T::NONE,
+                             INIT_T::HIGHER_AMOUNT,
+                             INIT_T::EARLIEST_DEADLINE,
+                             INIT_T::FURTHEST});
+  for (auto init : inits) {
+    for (float lambda = 0; lambda <= 3.05; lambda += 0.1) {
+      parameters.push_back({init, lambda});
+    }
+  }
+
+  auto P = parameters.size();
+  std::vector<tw_solution> tw_solutions(P);
+
+  // Split the work among threads.
+  std::vector<std::vector<std::size_t>>
+    thread_ranks(nb_threads, std::vector<std::size_t>());
+  for (std::size_t i = 0; i < P; ++i) {
+    thread_ranks[i % nb_threads].push_back(i);
+  }
+
+  auto run_solve = [&](const std::vector<std::size_t>& param_ranks) {
+    for (auto rank : param_ranks) {
+      auto& p = parameters[rank];
+      tw_solutions[rank] = solomon(_input, p.init, p.regret_coeff);
+    }
+  };
+
+  std::vector<std::thread> solving_threads;
+
+  for (std::size_t i = 0; i < nb_threads; ++i) {
+    solving_threads.emplace_back(run_solve, thread_ranks[i]);
+  }
+
+  for (auto& t : solving_threads) {
+    t.join();
+  }
+
+  std::vector<solution> solutions;
+  cost_t best_cost = std::numeric_limits<cost_t>::max();
+  std::size_t best_sol_rank;
+  for (const auto& tw_sol : tw_solutions) {
+    solutions.push_back(format_solution(_input, tw_sol));
+    if (solutions.back().summary.cost < best_cost) {
+      best_cost = solutions.back().summary.cost;
+      best_sol_rank = solutions.size() - 1;
+    }
+  }
+
+  return solutions[best_sol_rank];
 }
