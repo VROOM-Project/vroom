VROOM-Project/vroom

commit 5501acd701a67d9be4448caee7e528c8104d4fe6

Use lexicographic order for all amounts sorting. #150

src/problems/cvrp/local_search/local_search.cpp
@@ -550,8 +550,8 @@ void cvrp_local_search::run_ls_step() {
   while (best_gain > 0) {
     // Relocate stuff
     for (const auto& s_t : s_t_pairs) {
-      if (_input._vehicles[s_t.second].capacity <
-          total_amount(s_t.second) + _amount_lower_bound) {
+      if (total_amount(s_t.second) + _amount_lower_bound <=
+          _input._vehicles[s_t.second].capacity) {
         // Don't try to put things in a full vehicle.
         continue;
       }
@@ -594,8 +594,8 @@ void cvrp_local_search::run_ls_step() {
 
     // Or-opt stuff
     for (const auto& s_t : s_t_pairs) {
-      if (_input._vehicles[s_t.second].capacity <
-          total_amount(s_t.second) + _double_amount_lower_bound) {
+      if (total_amount(s_t.second) + _double_amount_lower_bound <=
+          _input._vehicles[s_t.second].capacity) {
         // Don't try to put things in a full vehicle.
         continue;
       }

src/problems/cvrp/heuristics/clustering.cpp
@@ -143,11 +143,11 @@ void clustering::parallel_clustering() {
   // Cluster initialization: define available initialization
   // strategies then run initialization sequentially on all clusters.
 
-  // Initialize cluster with the job that has higher amount (and is
+  // Initialize cluster with the job that has "higher" amount (and is
   // the further away in case of amount tie).
   auto higher_amount_init_lambda = [&](auto v) {
     return [&, v](index_t lhs, index_t rhs) {
-      return jobs[lhs].amount < jobs[rhs].amount or
+      return jobs[lhs].amount << jobs[rhs].amount or
              (jobs[lhs].amount == jobs[rhs].amount and
               costs[v][lhs] < costs[v][rhs]);
     };
@@ -254,11 +254,11 @@ void clustering::parallel_clustering() {
       auto current_j = candidates[v].back();
       if (jobs[current_j].amount <= capacities[v] and
           (costs[v][current_j] < best_cost or
-           (costs[v][current_j] == best_cost and
-            capacities[best_v] < capacities[v]))) {
+           (costs[v][current_j] == best_cost and capacities[best_v]
+                                                   << capacities[v]))) {
         // Update if job candidate is OK wrt capacity and cheaper to
-        // add. In case of cost tie, pick cluster with most remaining
-        // room.
+        // add. In case of cost tie, pick cluster with "higher"
+        // capacity.
         capacity_ok = true;
         best_v = v;
         best_j = current_j;
@@ -386,11 +386,11 @@ void clustering::sequential_clustering() {
 
   // Define available initialization strategies.
 
-  // Initialize cluster with the job that has higher amount (and is
+  // Initialize cluster with the job that has "higher" amount (and is
   // the further away in case of amount tie).
   auto higher_amount_init_lambda = [&](auto v) {
     return [&, v](index_t lhs, index_t rhs) {
-      return jobs[lhs].amount < jobs[rhs].amount or
+      return jobs[lhs].amount << jobs[rhs].amount or
              (jobs[lhs].amount == jobs[rhs].amount and
               vehicles_to_job_costs[v][lhs] < vehicles_to_job_costs[v][rhs]);
     };

src/problems/vrptw/heuristics/solomon.cpp
@@ -79,7 +79,7 @@ tw_solution solomon(const input& input, INIT_T init, float lambda) {
       index_t best_job_rank = 0;
       for (const auto& job_rank : unassigned) {
         if (!input.vehicle_ok_with_job(v, job_rank) or
-            vehicle.capacity < input._jobs[job_rank].amount) {
+            !(input._jobs[job_rank].amount <= vehicle.capacity)) {
           continue;
         }
 
@@ -90,7 +90,7 @@ tw_solution solomon(const input& input, INIT_T init, float lambda) {
         }
 
         if (init == INIT_T::HIGHER_AMOUNT and
-            higher_amount < input._jobs[job_rank].amount and
+            higher_amount << input._jobs[job_rank].amount and
             tw_r.is_valid_addition_for_tw(job_rank, 0)) {
           higher_amount = input._jobs[job_rank].amount;
           best_job_rank = job_rank;
@@ -109,7 +109,7 @@ tw_solution solomon(const input& input, INIT_T init, float lambda) {
           best_job_rank = job_rank;
         }
       }
-      if ((init == INIT_T::HIGHER_AMOUNT and route_amount < higher_amount) or
+      if ((init == INIT_T::HIGHER_AMOUNT and route_amount << higher_amount) or
           (init == INIT_T::EARLIEST_DEADLINE and
            earliest_deadline < std::numeric_limits<duration_t>::max()) or
           (init == INIT_T::FURTHEST and furthest_cost > 0)) {
@@ -128,7 +128,8 @@ tw_solution solomon(const input& input, INIT_T init, float lambda) {
 
       for (const auto& job_rank : unassigned) {
         if (!input.vehicle_ok_with_job(v, job_rank) or
-            vehicle.capacity < route_amount + input._jobs[job_rank].amount) {
+            !(route_amount + input._jobs[job_rank].amount <=
+              vehicle.capacity)) {
           continue;
         }
 

src/structures/vroom/amount.h
@@ -27,19 +27,25 @@ public:
   };
 };
 
+// Lexicographical comparison, useful for situations where a total
+// order is required.
 template <typename E1, typename E2>
-bool operator<(const amount_expression_t<E1>& lhs,
-               const amount_expression_t<E2>& rhs) {
-  bool is_strict_inf = true;
+bool operator<<(const amount_expression_t<E1>& lhs,
+                const amount_expression_t<E2>& rhs) {
   assert(lhs.size() == rhs.size());
-  for (std::size_t i = 0; i < lhs.size(); ++i) {
-    if (lhs[i] >= rhs[i]) {
-      is_strict_inf = false;
-      break;
+  if (lhs.empty()) {
+    return false;
+  }
+  auto last_rank = lhs.size() - 1;
+  for (std::size_t i = 0; i < last_rank; ++i) {
+    if (lhs[i] < rhs[i]) {
+      return true;
+    }
+    if (lhs[i] > rhs[i]) {
+      return false;
     }
   }
-
-  return is_strict_inf;
+  return lhs[last_rank] < rhs[last_rank];
 }
 
 template <typename E1, typename E2>

commit 903ed5caa990dfa7c1b04240c9929bbbf66a71a9

Fix regression introduced in 5501acd. #150

src/problems/cvrp/local_search/local_search.cpp
@@ -550,11 +550,8 @@ void cvrp_local_search::run_ls_step() {
   while (best_gain > 0) {
     // Exchange stuff
     for (const auto& s_t : s_t_pairs) {
-      if (s_t.second <= s_t.first) {
-        // This operator is symmetric.
-        continue;
-      }
-      if ((_sol[s_t.first].size() == 0) or (_sol[s_t.second].size() == 0)) {
+      if (s_t.second <= s_t.first or // This operator is symmetric.
+          _sol[s_t.first].size() == 0 or _sol[s_t.second].size() == 0) {
         continue;
       }
 
@@ -572,14 +569,11 @@ void cvrp_local_search::run_ls_step() {
 
     // CROSS-exchange stuff
     for (const auto& s_t : s_t_pairs) {
-      if (s_t.second <= s_t.first) {
-        // This operator is symmetric.
+      if (s_t.second <= s_t.first or // This operator is symmetric.
+          _sol[s_t.first].size() < 2 or _sol[s_t.second].size() < 2) {
         continue;
       }
 
-      if ((_sol[s_t.first].size() < 2) or (_sol[s_t.second].size() < 2)) {
-        continue;
-      }
       for (unsigned s_rank = 0; s_rank < _sol[s_t.first].size() - 1; ++s_rank) {
         for (unsigned t_rank = 0; t_rank < _sol[s_t.second].size() - 1;
              ++t_rank) {
@@ -639,12 +633,11 @@ void cvrp_local_search::run_ls_step() {
 
     // Relocate stuff
     for (const auto& s_t : s_t_pairs) {
-      if (total_amount(s_t.second) + _amount_lower_bound <=
-          _input._vehicles[s_t.second].capacity) {
-        // Don't try to put things in a full vehicle.
-        continue;
-      }
-      if (_sol[s_t.first].size() == 0) {
+      if (_sol[s_t.first].size() == 0 or
+          !(total_amount(s_t.second) + _amount_lower_bound <=
+            _input._vehicles[s_t.second].capacity)) {
+        // Don't try to put things in a full vehicle or from an empty
+        // vehicle.
         continue;
       }
       for (unsigned s_rank = 0; s_rank < _sol[s_t.first].size(); ++s_rank) {
@@ -667,12 +660,11 @@ void cvrp_local_search::run_ls_step() {
 
     // Or-opt stuff
     for (const auto& s_t : s_t_pairs) {
-      if (total_amount(s_t.second) + _double_amount_lower_bound <=
-          _input._vehicles[s_t.second].capacity) {
-        // Don't try to put things in a full vehicle.
-        continue;
-      }
-      if (_sol[s_t.first].size() < 2) {
+      if (_sol[s_t.first].size() < 2 or
+          !(total_amount(s_t.second) + _double_amount_lower_bound <=
+            _input._vehicles[s_t.second].capacity)) {
+        // Don't try to put things in a full vehicle or from a
+        // (near-)empty vehicle.
         continue;
       }
       for (unsigned s_rank = 0; s_rank < _sol[s_t.first].size() - 1; ++s_rank) {