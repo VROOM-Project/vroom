55a3d072cc508b75d8d569553ae244bbb562f3f3
jcoupey <git@coupey.fr>
2019-07-02 16:26:36 +0200

    Take priority sum into account for solution comparison.

--- src/structures/vroom/solution_state.h
+++ src/structures/vroom/solution_state.h
@@ -25,23 +25,30 @@ using RawSolution = std::vector<RawRoute>;
 using TWSolution = std::vector<TWRoute>;
 
 struct SolutionIndicators {
+  Priority priority_sum;
   unsigned unassigned;
   Cost cost;
   unsigned used_vehicles;
 
   friend bool operator<(const SolutionIndicators& lhs,
                         const SolutionIndicators& rhs) {
-    if (lhs.unassigned < rhs.unassigned) {
+    if (lhs.priority_sum > rhs.priority_sum) {
       return true;
     }
-    if (lhs.unassigned == rhs.unassigned) {
-      if (lhs.cost < rhs.cost) {
+    if (lhs.priority_sum == rhs.priority_sum) {
+      if (lhs.unassigned < rhs.unassigned) {
         return true;
       }
-      if (lhs.cost == rhs.cost and lhs.used_vehicles < rhs.used_vehicles) {
-        return true;
+      if (lhs.unassigned == rhs.unassigned) {
+        if (lhs.cost < rhs.cost) {
+          return true;
+        }
+        if (lhs.cost == rhs.cost and lhs.used_vehicles < rhs.used_vehicles) {
+          return true;
+        }
       }
     }
+
     return false;
   }
 };
