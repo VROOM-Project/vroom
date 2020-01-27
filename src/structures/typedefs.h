d094176a9df0f9989543af952d00b3621336054b
jcoupey <git@coupey.fr>
2020-01-27 09:43:06 +0100

    Adjust priority scale.

--- src/structures/typedefs.h
+++ src/structures/typedefs.h
@@ -43,7 +43,7 @@ constexpr Cost INFINITE_COST = 3 * (std::numeric_limits<Cost>::max() / 4);
 
 const std::string DEFAULT_PROFILE = "car";
 
-constexpr Priority MAX_PRIORITY = 10;
+constexpr Priority MAX_PRIORITY = 100;
 
 // Available routing engines.
 enum class ROUTER { OSRM, LIBOSRM, ORS };
