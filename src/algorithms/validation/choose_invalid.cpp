c107c3e3978d4403da536fb6817286d92dfab675
jcoupey <git@coupey.fr>
2020-10-27 17:17:39 +0100

    Define a MIP to pick job time window.

--- src/algorithms/validation/choose_invalid.cpp
+++ src/algorithms/validation/choose_invalid.cpp
@@ -6,12 +6,13 @@ Copyright (c) 2015-2019, Julien Coupey.
 All rights reserved (see LICENSE).
 
 */
-
 #include <iostream>
 
-#include "algorithms/validation/choose_invalid.h"
+#include <numeric>
 
 #include <glpk.h>
+
+#include "algorithms/validation/choose_invalid.h"
 
 namespace vroom {
 namespace validation {
@@ -21,10 +22,19 @@ Route choose_invalid_route(const Input& input,
                            const std::vector<Index>& job_ranks,
                            std::unordered_set<Index>& unassigned_ranks) {
   const auto& m = input.get_matrix();
-  unsigned n = job_ranks.size();
+  const unsigned n = job_ranks.size();
   const auto& v = input.vehicles[vehicle_rank];
 
   constexpr double M = 10.0;
+
+  // Total number of time windows.
+  const unsigned K =
+    std::accumulate(job_ranks.begin(),
+                    job_ranks.end(),
+                    0,
+                    [&](auto sum, auto job_rank) {
+                      return sum + input.jobs[job_rank].tws.size();
+                    });
 
   // Create problem.
   glp_prob* lp;
@@ -32,26 +42,22 @@ Route choose_invalid_route(const Input& input,
   glp_set_prob_name(lp, "choose_ETA");
   glp_set_obj_dir(lp, GLP_MIN);
 
-  // Define constraints.
-  unsigned nb_constraints = 3 * n + 1;
-  double first = 0;
-  if (v.has_start()) {
-    ++nb_constraints;
-    // Take into account time from start point to first job.
-    first = m[v.start.value().index()][input.jobs[job_ranks.front()].index()];
-  }
-  double last = 0;
-  if (v.has_end()) {
-    ++nb_constraints;
-    // Take into account time from last job arrival to end point.
-    last = m[input.jobs[job_ranks.back()].index()][v.end.value().index()] +
-           input.jobs[job_ranks.back()].service;
-  }
+  // Define constraints and remember number of non-zero values in the
+  // matrix.
+  const unsigned nb_constraints = 4 * n + 3;
+  const unsigned nb_non_zero = 2 * (3 * n + 3) + 3 * K;
+
   glp_add_rows(lp, nb_constraints);
 
   unsigned current_row = 1;
+
   // Precedence constraints.
-  glp_set_row_name(lp, current_row, "p0");
+  double first = 0;
+  if (v.has_start()) {
+    // Take into account time from start point to first job.
+    first = m[v.start.value().index()][input.jobs[job_ranks.front()].index()];
+  }
+  glp_set_row_name(lp, current_row, "P0");
   glp_set_row_bnds(lp, current_row, GLP_LO, first, 0.0);
   ++current_row;
 
@@ -59,55 +65,67 @@ Route choose_invalid_route(const Input& input,
     double dist =
       m[input.jobs[job_ranks[i]].index()][input.jobs[job_ranks[i + 1]].index()];
     double service = input.jobs[job_ranks[i]].service;
-    auto name = "p" + std::to_string(i + 1);
+    auto name = "P" + std::to_string(i + 1);
     glp_set_row_name(lp, current_row, name.c_str());
     glp_set_row_bnds(lp, current_row, GLP_LO, dist + service, 0.0);
     ++current_row;
   }
-  glp_set_row_name(lp, current_row, ("p" + std::to_string(n)).c_str());
+
+  double last = 0;
+  if (v.has_end()) {
+    // Take into account time from last job arrival to end point.
+    last = m[input.jobs[job_ranks.back()].index()][v.end.value().index()] +
+           input.jobs[job_ranks.back()].service;
+  }
+  glp_set_row_name(lp, current_row, ("P" + std::to_string(n)).c_str());
   glp_set_row_bnds(lp, current_row, GLP_LO, last, 0.0);
   ++current_row;
 
+  assert(current_row == n + 2);
+
   // Vehicle TW start violation constraint.
-  if (v.has_start()) {
-    assert(current_row == n + 2);
-    double lb = v.tw.start;
-    glp_set_row_name(lp, current_row, "e0");
-    glp_set_row_bnds(lp, current_row, GLP_LO, lb, 0.0);
+  double lb = v.tw.start;
+  glp_set_row_name(lp, current_row, "L0");
+  glp_set_row_bnds(lp, current_row, GLP_LO, lb, 0.0);
+  ++current_row;
+
+  // Lead time ("earliest violation") constraints.
+  for (unsigned i = 0; i < n; ++i) {
+    auto name = "L" + std::to_string(i + 1);
+    glp_set_row_name(lp, current_row, name.c_str());
+    glp_set_row_bnds(lp, current_row, GLP_LO, 0.0, 0.0);
     ++current_row;
   }
+  assert(current_row == 2 * n + 3);
 
-  // "Earliest violation" constraints.
+  // Delay ("latest violation") constraints.
   for (unsigned i = 0; i < n; ++i) {
-    double lb = input.jobs[job_ranks[i]].tws[0].start;
-    auto name = "e" + std::to_string(i + 1);
+    auto name = "D" + std::to_string(i + 1);
     glp_set_row_name(lp, current_row, name.c_str());
-    glp_set_row_bnds(lp, current_row, GLP_LO, lb, 0.0);
-    ++current_row;
-  }
-
-  // "Latest violation" constraints.
-  for (unsigned i = 0; i < n; ++i) {
-    double ub = input.jobs[job_ranks[i]].tws[0].end;
-    auto name = "l" + std::to_string(i + 1);
-    glp_set_row_name(lp, current_row, name.c_str());
-    glp_set_row_bnds(lp, current_row, GLP_UP, 0.0, ub);
+    glp_set_row_bnds(lp, current_row, GLP_UP, 0.0, 0.0);
     ++current_row;
   }
 
   // Vehicle TW end violation constraint.
-  if (v.has_end()) {
-    double ub = v.tw.end;
-    auto name = "l" + std::to_string(n + 1);
+  double ub = v.tw.end;
+  auto name = "L" + std::to_string(n + 1);
+  glp_set_row_name(lp, current_row, name.c_str());
+  glp_set_row_bnds(lp, current_row, GLP_UP, 0.0, ub);
+  ++current_row;
+
+  assert(current_row == 3 * n + 4);
+
+  // Binary variable decision constraints.
+  for (unsigned i = 1; i <= n; ++i) {
+    auto name = "s" + std::to_string(i);
     glp_set_row_name(lp, current_row, name.c_str());
-    glp_set_row_bnds(lp, current_row, GLP_UP, 0.0, ub);
+    glp_set_row_bnds(lp, current_row, GLP_FX, 1.0, 1.0);
     ++current_row;
   }
-
   assert(current_row == nb_constraints + 1);
 
   // Set variables and coefficients.
-  unsigned nb_var = 2 * n + 4;
+  const unsigned nb_var = 2 * n + 4 + K;
   glp_add_cols(lp, nb_var);
 
   unsigned current_col = 1;
@@ -132,103 +150,156 @@ Route choose_invalid_route(const Input& input,
     glp_set_obj_coef(lp, current_col, M);
     ++current_col;
   }
+  assert(current_col == 2 * n + 5);
 
+  // Binary variables for job time window choice.
+  for (unsigned i = 0; i < n; ++i) {
+    for (unsigned k = 0; k < input.jobs[job_ranks[i]].tws.size(); ++k) {
+      auto name = "X" + std::to_string(i + 1) + "_" + std::to_string(k);
+      glp_set_col_name(lp, current_col, name.c_str());
+      glp_set_col_kind(lp, current_col, GLP_BV);
+      ++current_col;
+    }
+  }
   assert(current_col == nb_var + 1);
 
   // Define non-zero elements in matrix.
-  unsigned nb_non_zero = 2 * nb_constraints;
   int* ia = new int[1 + nb_non_zero];
   int* ja = new int[1 + nb_non_zero];
   double* ar = new double[1 + nb_non_zero];
 
-  unsigned current_rank = 1;
+  unsigned r = 1;
   // Coefficients for precedence constraints.
   for (unsigned i = 1; i <= n + 1; ++i) {
     // a[i,i] = -1
-    ia[current_rank] = i;
-    ja[current_rank] = i;
-    ar[current_rank] = -1;
-    ++current_rank;
+    ia[r] = i;
+    ja[r] = i;
+    ar[r] = -1;
+    ++r;
 
     // a[i,i + 1] = 1
-    ia[current_rank] = i;
-    ja[current_rank] = i + 1;
-    ar[current_rank] = 1;
-    ++current_rank;
+    ia[r] = i;
+    ja[r] = i + 1;
+    ar[r] = 1;
+    ++r;
   }
 
   unsigned constraint_rank = n + 2;
 
-  // Coefficients for vehicle "earliest violation" constraint.
-  if (v.has_start()) {
-    // a[constraint_rank, 1] = 1
-    ia[current_rank] = constraint_rank;
-    ja[current_rank] = 1;
-    ar[current_rank] = 1;
-    ++current_rank;
+  // Coefficients for L0 constraint.
+  // a[constraint_rank, 1] = 1
+  ia[r] = constraint_rank;
+  ja[r] = 1;
+  ar[r] = 1;
+  ++r;
 
-    // a[constraint_rank, n + 3] = 1
-    ia[current_rank] = constraint_rank;
-    ja[current_rank] = n + 3;
-    ar[current_rank] = 1;
-    ++current_rank;
+  // a[constraint_rank, n + 3] = 1
+  ia[r] = constraint_rank;
+  ja[r] = n + 3;
+  ar[r] = 1;
+  ++r;
+  ++constraint_rank;
 
-    ++constraint_rank;
-  }
+  // Coefficients other L_i constraints. current_X_rank is the rank
+  // for binaries that describe the time window choices.
+  unsigned current_X_rank = 2 * n + 4 + 1;
 
-  // Coefficients for "earliest violations" constraints.
   for (unsigned i = 2; i <= n + 1; ++i) {
     // a[constraint_rank, i] = 1
-    ia[current_rank] = constraint_rank;
-    ja[current_rank] = i;
-    ar[current_rank] = 1;
-    ++current_rank;
+    ia[r] = constraint_rank;
+    ja[r] = i;
+    ar[r] = 1;
+    ++r;
 
     // a[constraint_rank, n + 2 + i] = 1
-    ia[current_rank] = constraint_rank;
-    ja[current_rank] = n + 2 + i;
-    ar[current_rank] = 1;
-    ++current_rank;
+    ia[r] = constraint_rank;
+    ja[r] = n + 2 + i;
+    ar[r] = 1;
+    ++r;
+
+    const auto& job = input.jobs[job_ranks[i - 2]];
+    for (const auto& tw : job.tws) {
+      // a[constraint_rank, current_X_rank] = - earliest_date for k-th TW of job
+      ia[r] = constraint_rank;
+      ja[r] = current_X_rank;
+      ar[r] = -static_cast<double>(tw.start);
+      ++r;
+
+      ++current_X_rank;
+    }
 
     ++constraint_rank;
   }
+  assert(current_X_rank == 2 * n + 4 + K + 1);
+  assert(constraint_rank == 2 * n + 3);
 
-  // Coefficients for "latest violations" constraints.
+  // Coefficients for D_i constraints.
+  current_X_rank = 2 * n + 4 + 1;
+
   for (unsigned i = 2; i <= n + 1; ++i) {
     // a[constraint_rank, i] = 1
-    ia[current_rank] = constraint_rank;
-    ja[current_rank] = i;
-    ar[current_rank] = 1;
-    ++current_rank;
+    ia[r] = constraint_rank;
+    ja[r] = i;
+    ar[r] = 1;
+    ++r;
 
-    // a[n + i, n + 2 + i] = -1
-    ia[current_rank] = constraint_rank;
-    ja[current_rank] = n + 2 + i;
-    ar[current_rank] = -1;
-    ++current_rank;
+    // a[constraint_rank, n + 2 + i] = -1
+    ia[r] = constraint_rank;
+    ja[r] = n + 2 + i;
+    ar[r] = -1;
+    ++r;
 
-    ++constraint_rank;
-  }
+    const auto& job = input.jobs[job_ranks[i - 2]];
+    for (const auto& tw : job.tws) {
+      // a[constraint_rank, current_X_rank] = - latest_date for k-th TW of job
+      ia[r] = constraint_rank;
+      ja[r] = current_X_rank;
+      ar[r] = -static_cast<double>(tw.end);
+      ++r;
 
-  // Coefficients for vehicle "latest violation" constraint.
-  if (v.has_end()) {
-    // a[constraint_rank, n + 2] = 1
-    ia[current_rank] = constraint_rank;
-    ja[current_rank] = n + 2;
-    ar[current_rank] = 1;
-    ++current_rank;
-
-    // a[constraint_rank, 2 * n + 4] = -1
-    ia[current_rank] = constraint_rank;
-    ja[current_rank] = 2 * n + 4;
-    ar[current_rank] = -1;
-    ++current_rank;
+      ++current_X_rank;
+    }
 
     ++constraint_rank;
   }
+  assert(current_X_rank == 2 * n + 4 + K + 1);
 
+  // Coefficients D_{n + 1} constraint.
+  // a[constraint_rank, n + 2] = 1
+  ia[r] = constraint_rank;
+  ja[r] = n + 2;
+  ar[r] = 1;
+  ++r;
+
+  // a[constraint_rank, 2 * n + 4] = -1
+  ia[r] = constraint_rank;
+  ja[r] = 2 * n + 4;
+  ar[r] = -1;
+  ++r;
+  ++constraint_rank;
+
+  assert(constraint_rank == 3 * n + 4);
+
+  // Decision constraints S_i for binary variables.
+  current_X_rank = 2 * n + 4 + 1;
+
+  for (unsigned i = 0; i < n; ++i) {
+    const auto& job = input.jobs[job_ranks[i]];
+    for (unsigned k = 0; k < job.tws.size(); ++k) {
+      // a[constraint_rank, current_X_rank] = 1
+      ia[r] = constraint_rank;
+      ja[r] = current_X_rank;
+      ar[r] = 1;
+      ++r;
+
+      ++current_X_rank;
+    }
+    ++constraint_rank;
+  }
+  assert(current_X_rank == 2 * n + 4 + K + 1);
   assert(constraint_rank == nb_constraints + 1);
-  assert(current_rank == nb_non_zero + 1);
+
+  assert(r == nb_non_zero + 1);
 
   glp_load_matrix(lp, nb_non_zero, ia, ja, ar);
 
@@ -237,21 +308,46 @@ Route choose_invalid_route(const Input& input,
   delete[] ar;
 
   // Solve.
-  glp_simplex(lp, NULL);
+  glp_iocp parm;
+  glp_init_iocp(&parm);
+  parm.presolve = GLP_ON;
+  glp_intopt(lp, &parm);
+
+  // glp_print_mip(lp, "mip.sol");
 
   // Get output.
   std::cout << "- Obj: " << glp_get_obj_val(lp) << std::endl;
 
-  auto v_start = glp_get_col_prim(lp, 1);
-  auto v_end = glp_get_col_prim(lp, n + 2);
+  auto v_start = glp_mip_col_val(lp, 1);
+  auto v_end = glp_mip_col_val(lp, n + 2);
 
   std::cout << "start: " << v_start << std::endl;
   std::vector<Duration> job_ETA;
   for (unsigned i = 2; i <= n + 1; ++i) {
-    job_ETA.push_back(glp_get_col_prim(lp, i));
+    job_ETA.push_back(glp_mip_col_val(lp, i));
+
     std::cout << "t" << i - 1 << ": " << job_ETA.back() << std::endl;
   }
   std::cout << "end: " << v_end << std::endl;
+
+  // Populate vector storing picked time window ranks.
+  current_X_rank = 2 * n + 4 + 1;
+  std::vector<Index> job_tw_ranks;
+
+  for (unsigned i = 0; i < n; ++i) {
+    const auto& job = input.jobs[job_ranks[i]];
+    for (unsigned k = 0; k < job.tws.size(); ++k) {
+      auto val = glp_mip_col_val(lp, current_X_rank);
+      if (val == 1) {
+        job_tw_ranks.push_back(k);
+      }
+      std::cout << "X" << i + 1 << "_" << k << ": " << val << std::endl;
+
+      ++current_X_rank;
+    }
+    assert(job_tw_ranks.size() == i + 1);
+  }
+  assert(current_X_rank == 2 * n + 4 + K + 1);
 
   glp_delete_prob(lp);
   glp_free_env();
@@ -318,13 +414,14 @@ Route choose_invalid_route(const Input& input,
   first_step.waiting_time = wt;
   forward_wt += wt;
 
-  if (service_start < first_job.tws[0].start) {
-    Duration lt = first_job.tws[0].start - service_start;
+  auto first_tw_rank = job_tw_ranks.front();
+  if (service_start < first_job.tws[first_tw_rank].start) {
+    Duration lt = first_job.tws[first_tw_rank].start - service_start;
     first_step.lead_time = lt;
     lead_time += lt;
   }
-  if (first_job.tws[0].end < service_start) {
-    Duration dl = service_start - first_job.tws[0].end;
+  if (first_job.tws[first_tw_rank].end < service_start) {
+    Duration dl = service_start - first_job.tws[first_tw_rank].end;
     first_step.delay = dl;
     delay += dl;
   }
@@ -334,6 +431,7 @@ Route choose_invalid_route(const Input& input,
   for (std::size_t r = 0; r < job_ranks.size() - 1; ++r) {
     const auto& previous_job = input.jobs[job_ranks[r]];
     const auto& next_job = input.jobs[job_ranks[r + 1]];
+    const auto& next_tw_rank = job_tw_ranks[r + 1];
 
     Duration travel = m[previous_job.index()][next_job.index()];
     duration += travel;
@@ -359,13 +457,13 @@ Route choose_invalid_route(const Input& input,
     current.waiting_time = wt;
     forward_wt += wt;
 
-    if (service_start < next_job.tws[0].start) {
-      Duration lt = next_job.tws[0].start - service_start;
+    if (service_start < next_job.tws[next_tw_rank].start) {
+      Duration lt = next_job.tws[next_tw_rank].start - service_start;
       current.lead_time = lt;
       lead_time += lt;
     }
-    if (next_job.tws[0].end < service_start) {
-      Duration dl = service_start - next_job.tws[0].end;
+    if (next_job.tws[next_tw_rank].end < service_start) {
+      Duration dl = service_start - next_job.tws[next_tw_rank].end;
       current.delay = dl;
       delay += dl;
     }
