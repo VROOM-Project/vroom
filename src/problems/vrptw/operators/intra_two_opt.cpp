33bf4da7ab3b3a7ade40d661e6cfb303d036c8a0
jcoupey <git@coupey.fr>
2022-05-02 15:52:14 +0200

    Sketch for vrptw::IntraTwoOpt.

---
+++ src/problems/vrptw/operators/intra_two_opt.cpp
@@ -0,0 +1,40 @@
+/*
+
+This file is part of VROOM.
+
+Copyright (c) 2015-2022, Julien Coupey.
+All rights reserved (see LICENSE).
+
+*/
+
+#include "problems/vrptw/operators/intra_two_opt.h"
+
+namespace vroom {
+namespace vrptw {
+
+IntraTwoOpt::IntraTwoOpt(const Input& input,
+                         const utils::SolutionState& sol_state,
+                         TWRoute& tw_s_route,
+                         Index s_vehicle,
+                         Index s_rank,
+                         Index t_rank)
+  : cvrp::IntraTwoOpt(input,
+                      sol_state,
+                      static_cast<RawRoute&>(tw_s_route),
+                      s_vehicle,
+                      s_rank,
+                      t_rank),
+    _tw_s_route(tw_s_route) {
+}
+
+bool IntraTwoOpt::is_valid() {
+  // TODO
+  return true;
+}
+
+void IntraTwoOpt::apply() {
+  // TODO
+}
+
+} // namespace vrptw
+} // namespace vroom
