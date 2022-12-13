57d33cddc5a5c684a8f2c7ecda456d614bfeb25d
jcoupey <git@coupey.fr>
2022-12-13 14:49:59 +0100

    Do not use wrong delivery sum after target modification in RouteExchange.

--- src/problems/vrptw/operators/route_exchange.cpp
+++ src/problems/vrptw/operators/route_exchange.cpp
@@ -25,21 +25,23 @@ RouteExchange::RouteExchange(const Input& input,
                         static_cast<RawRoute&>(tw_t_route),
                         t_vehicle),
     _tw_s_route(tw_s_route),
-    _tw_t_route(tw_t_route) {
+    _tw_t_route(tw_t_route),
+    _source_job_deliveries_sum(source.job_deliveries_sum()),
+    _target_job_deliveries_sum(target.job_deliveries_sum()) {
 }
 
 bool RouteExchange::is_valid() {
   bool valid = cvrp::RouteExchange::is_valid();
   valid =
     valid && _tw_t_route.is_valid_addition_for_tw(_input,
-                                                  source.job_deliveries_sum(),
+                                                  _source_job_deliveries_sum,
                                                   s_route.begin(),
                                                   s_route.end(),
                                                   0,
                                                   t_route.size());
   valid =
     valid && _tw_s_route.is_valid_addition_for_tw(_input,
-                                                  target.job_deliveries_sum(),
+                                                  _target_job_deliveries_sum,
                                                   t_route.begin(),
                                                   t_route.end(),
                                                   0,
@@ -54,7 +56,7 @@ void RouteExchange::apply() {
     _tw_t_route.remove(_input, 0, t_route.size());
   } else {
     _tw_t_route.replace(_input,
-                        source.job_deliveries_sum(),
+                        _source_job_deliveries_sum,
                         s_route.begin(),
                         s_route.end(),
                         0,
@@ -65,7 +67,7 @@ void RouteExchange::apply() {
     _tw_s_route.remove(_input, 0, s_route.size());
   } else {
     _tw_s_route.replace(_input,
-                        target.job_deliveries_sum(),
+                        _target_job_deliveries_sum,
                         t_job_ranks.begin(),
                         t_job_ranks.end(),
                         0,
