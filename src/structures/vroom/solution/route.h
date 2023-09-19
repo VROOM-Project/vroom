94c9908ff06d4d01a3f92b02bfa3d7ee48d37b59
jcoupey <git@coupey.fr>
2023-09-19 16:14:17 +0200

    Adjust move logic around Violations.

--- src/structures/vroom/solution/route.h
+++ src/structures/vroom/solution/route.h
@@ -49,7 +49,7 @@ struct Route {
         Amount pickup,
         std::string profile,
         std::string description,
-        const Violations&& violations = Violations(0, 0));
+        Violations&& violations = Violations(0, 0));
 
   void check_timing_consistency() const;
 };
