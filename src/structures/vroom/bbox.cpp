d630803cfd8cac50ac2163c5fa07e968e11c2e69
jcoupey <git@coupey.fr>
2023-09-29 20:44:38 +0200

    Add a bounding box class.

---
+++ src/structures/vroom/bbox.cpp
@@ -0,0 +1,27 @@
+/*
+
+This file is part of VROOM.
+
+Copyright (c) 2015-2022, Julien Coupey.
+All rights reserved (see LICENSE).
+
+*/
+
+#include "structures/vroom/bbox.h"
+
+namespace vroom {
+
+void BBox::extend(Coordinates c) {
+  min.lon = std::min(min.lon, c.lon);
+  min.lat = std::min(min.lat, c.lat);
+
+  max.lon = std::max(max.lon, c.lon);
+  max.lat = std::max(max.lat, c.lat);
+}
+
+bool BBox::intersects(const BBox& other) const {
+  return other.min.lon <= max.lon && other.min.lat <= max.lat &&
+         min.lon <= other.max.lon && min.lat <= other.max.lat;
+}
+
+} // namespace vroom
