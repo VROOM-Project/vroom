864f6607f6078596b4ec8b9bb5be9f31362a5f6c
jcoupey <git@coupey.fr>
2023-11-17 16:14:35 +0100

    Cherry-pick changes for #1020 fix.

--- src/structures/typedefs.h
+++ src/structures/typedefs.h
@@ -190,19 +190,22 @@ struct StringHash {
 };
 
 namespace utils {
-inline Duration scale_from_user_duration(UserDuration d) {
+constexpr inline Duration scale_from_user_duration(UserDuration d) {
   return DURATION_FACTOR * static_cast<Duration>(d);
 }
 
-inline UserDuration scale_to_user_duration(Duration d) {
+constexpr inline UserDuration scale_to_user_duration(Duration d) {
+  assert(d <=
+         scale_from_user_duration(std::numeric_limits<UserDuration>::max()));
   return static_cast<UserDuration>(d / DURATION_FACTOR);
 }
 
-inline Cost scale_from_user_cost(UserCost c) {
+constexpr inline Cost scale_from_user_cost(UserCost c) {
   return DURATION_FACTOR * COST_FACTOR * static_cast<Cost>(c);
 }
 
-inline UserCost scale_to_user_cost(Cost c) {
+constexpr inline UserCost scale_to_user_cost(Cost c) {
+  assert(c <= scale_from_user_cost(std::numeric_limits<UserCost>::max()));
   return static_cast<UserCost>(c / (DURATION_FACTOR * COST_FACTOR));
 }
 } // namespace utils
