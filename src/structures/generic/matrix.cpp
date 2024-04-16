4f8b861baa2d5f115684b00e3599e50357ebc418
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2024-04-16 14:21:28 +0200

    Move function definition to header file

--- src/structures/generic/matrix.cpp
+++ src/structures/generic/matrix.cpp
@@ -11,17 +11,6 @@ All rights reserved (see LICENSE).
 
 namespace vroom {
 
-template <class T>
-Matrix<T> Matrix<T>::get_sub_matrix(const std::vector<Index>& indices) const {
-  Matrix<T> sub_matrix(indices.size());
-  for (std::size_t i = 0; i < indices.size(); ++i) {
-    for (std::size_t j = 0; j < indices.size(); ++j) {
-      sub_matrix[i][j] = (*this)[indices[i]][indices[j]];
-    }
-  }
-  return sub_matrix;
-}
-
 template class Matrix<UserCost>;
 
 } // namespace vroom
--- src/structures/generic/matrix.h
+++ src/structures/generic/matrix.h
@@ -38,7 +38,15 @@ public:
   {
   }
 
-  Matrix<T> get_sub_matrix(const std::vector<Index>& indices) const;
+  Matrix<T> get_sub_matrix(const std::vector<Index>& indices) const {
+    Matrix<T> sub_matrix(indices.size());
+    for (std::size_t i = 0; i < indices.size(); ++i) {
+      for (std::size_t j = 0; j < indices.size(); ++j) {
+        sub_matrix[i][j] = (*this)[indices[i]][indices[j]];
+      }
+    }
+    return sub_matrix;
+  }
 
   T* operator[](std::size_t i) {
     return data.data() + (i * n);
