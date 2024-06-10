2e6245f1e90bc132c1a97da477028b9b146994ee
jcoupey <git@coupey.fr>
2024-06-10 17:30:55 +0200

    First sketch for LOG_LS_SEARCH structs.

---
+++ src/algorithms/local_search/log_local_search.h
@@ -0,0 +1,30 @@
+#ifndef LOG_LOCAL_SEARCH_H
+#define LOG_LOCAL_SEARCH_H
+
+/*
+
+This file is part of VROOM.
+
+Copyright (c) 2015-2024, Julien Coupey.
+All rights reserved (see LICENSE).
+
+*/
+
+namespace vroom::ls::log {
+
+enum class EVENT { Start };
+
+template <class Route> struct Step {
+  TimePoint time_point;
+  EVENT event;
+  OperatorName operator_name;
+  vroom::utils::SolutionIndicators<Route> indicators;
+};
+
+template <class Route> struct Dump {
+  HeuristicParameters heuristic_parameters;
+  std::vector<Step<Route>> steps;
+};
+} // namespace vroom::ls::log
+
+#endif
