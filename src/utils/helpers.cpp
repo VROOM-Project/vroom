9ea05f453b58d8ab2640407ec429418d04c826df
jcoupey <git@coupey.fr>
2024-09-16 16:15:04 +0200

    Partial cherry-pick of 498438a0.

--- src/utils/helpers.cpp
+++ src/utils/helpers.cpp
@@ -230,7 +230,7 @@ void check_priority(const Priority priority,
   }
 }
 
-std::vector<Job> get_unassigned_jobs_from_ranks(
+inline std::vector<Job> get_unassigned_jobs_from_ranks(
   const Input& input,
   const std::unordered_set<Index>& unassigned_ranks) {
   std::vector<Job> unassigned_jobs;
@@ -397,8 +397,7 @@ Solution format_solution(const Input& input, const RawSolution& raw_routes) {
 
   return Solution(input.zero_amount(),
                   std::move(routes),
-                  std::move(
-                    get_unassigned_jobs_from_ranks(input, unassigned_ranks)));
+                  get_unassigned_jobs_from_ranks(input, unassigned_ranks));
 }
 
 Route format_route(const Input& input,
@@ -900,8 +899,7 @@ Solution format_solution(const Input& input, const TWSolution& tw_routes) {
 
   return Solution(input.zero_amount(),
                   std::move(routes),
-                  std::move(
-                    get_unassigned_jobs_from_ranks(input, unassigned_ranks)));
+                  get_unassigned_jobs_from_ranks(input, unassigned_ranks));
 }
 
 } // namespace vroom::utils
