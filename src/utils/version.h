d1f7c8b317861d2736b8284269d0f938a055ce24
Zach Aries <Zach Aries>
2026-03-20 14:02:44 -0600

    trexity: bump edition to 202603201402 (v1.15.0)

--- src/utils/version.h
+++ src/utils/version.h
@@ -18,7 +18,7 @@ constexpr unsigned MINOR = 15;
 constexpr unsigned PATCH = 0;
 constexpr bool DEV = true;
 constexpr unsigned RC = 0;
-constexpr std::string_view TREXITY_EDITION = "202602262014";
+constexpr std::string_view TREXITY_EDITION = "202603201402";
 
 namespace vroom {
 
