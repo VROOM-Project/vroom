53bcb4ea17a20dfd9ab362169adeef95ba635399
Mathieu Bouchard <mat@trexity.com>
2026-03-23 13:25:35 -0400

    trexity: bump edition to 202603231325 (v1.15.0)

--- src/utils/version.h
+++ src/utils/version.h
@@ -18,7 +18,7 @@ constexpr unsigned MINOR = 15;
 constexpr unsigned PATCH = 0;
 constexpr bool DEV = true;
 constexpr unsigned RC = 0;
-constexpr std::string_view TREXITY_EDITION = "202603201402";
+constexpr std::string_view TREXITY_EDITION = "202603231325";
 
 namespace vroom {
 
